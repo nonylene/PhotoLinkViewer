0b400f20dcf5674122648730c628573e3f5076a7
nonylene <cookie.speaker@gmail.com>
2014-10-16 02:23:20 +0900

    TransParent

---
+++ app/src/main/java/nonylene/net/photolinkviewer/AsyncHttp.java
@@ -0,0 +1,73 @@
+package nonylene.net.photolinkviewer;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+
+public class AsyncHttp extends AsyncTaskLoader<Drawable> {
+
+    private URL url;
+    private Context context = null;
+    private Drawable result;
+
+    public AsyncHttp(Context context, URL url) {
+        super(context);
+        this.url = url;
+    }
+
+    @Override
+    public Drawable loadInBackground() {
+        Drawable drawable = null;
+
+        try {
+            InputStream inputStream = url.openStream();
+            drawable = Drawable.createFromStream(inputStream, "webimg");
+            inputStream.close();
+            return drawable;
+        } catch (IOException e) {
+            return drawable;
+        }
+    }
+
+    @Override
+    public void deliverResult(Drawable drawable){
+        if (isReset()){
+            if(this.result != null ){
+                this.result = null;
+            }
+            return;
+        }
+        this.result = drawable;
+        if (isStarted()){
+            super.deliverResult(drawable);
+        }
+    }
+
+    @Override
+    public void onStartLoading() {
+        if (this.result != null){
+            deliverResult(this.result);
+        }
+        if(takeContentChanged() || this.result == null){
+            forceLoad();
+        }
+    }
+
+    @Override
+    public void onStopLoading(){
+        super.onStopLoading();
+        cancelLoad();
+    }
+
+    @Override
+    public void onReset(){
+        super.onReset();
+        onStopLoading();
+    }
+}
--- app/src/main/java/nonylene/net/photolinkviewer/Show.java
+++ app/src/main/java/nonylene/net/photolinkviewer/Show.java
@@ -1,9 +1,21 @@
 package nonylene.net.photolinkviewer;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.URL;
 
 
 public class Show extends Activity {
@@ -12,6 +24,8 @@ public class Show extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+        AsyncExecute hoge = new AsyncExecute();
+        hoge.Start("https://pbs.twimg.com/media/Bz1FnXUCEAAkVGt.png:orig");
     }
 
 
@@ -33,4 +47,40 @@ public class Show extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    public class AsyncExecute implements LoaderManager.LoaderCallbacks<Drawable> {
+
+
+        public void Start(String url) {
+            Bundle bundle = new Bundle();
+            bundle.putString("url", url);
+            getLoaderManager().initLoader(0, bundle, this);
+        }
+
+        @Override
+        public Loader<Drawable> onCreateLoader(int id, Bundle bundle) {
+            try {
+                String c = bundle.getString("url");
+                URL url = new URL(c);
+                return new AsyncHttp(getApplicationContext(), url);
+
+            } catch (IOException e) {
+                Toast.makeText(Show.this, "fuck", Toast.LENGTH_LONG).show();
+                return null;
+            }
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Drawable> loader, Drawable drawable) {
+            ImageView imageView = (ImageView) findViewById(R.id.imgview);
+            imageView.setImageDrawable(drawable);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Drawable> loader) {
+
+        }
+    }
+
 }
