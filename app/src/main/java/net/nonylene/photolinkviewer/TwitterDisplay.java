491c2617d5e55a69eb9a40b11bf70c57bedb2b69
nonylene <cookie.speaker@gmail.com>
2014-11-11 17:45:30 +0900

    create twitter activity

---
+++ app/src/main/java/net/nonylene/photolinkviewer/TwitterDisplay.java
@@ -0,0 +1,15 @@
+package net.nonylene.photolinkviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class TwitterDisplay extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twitter_display);
+    }
+
+}
