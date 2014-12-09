da5c460e11c407131aa3b08b3d1bd3c47dc3475a
nonylene <cookie.speaker@gmail.com>
2014-12-09 20:52:16 +0900

    remove initial dialog

--- app/src/main/java/net/nonylene/photolinkviewer/Show.java
+++ app/src/main/java/net/nonylene/photolinkviewer/Show.java
@@ -1,13 +1,8 @@
 package net.nonylene.photolinkviewer;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.app.FragmentTransaction;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -18,12 +13,6 @@ public class Show extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
-        SharedPreferences preferences = getSharedPreferences("preference", MODE_PRIVATE);
-        if (!preferences.getBoolean("Initialized", false)) {
-            DialogFragment fragment = new InitDialogFragment();
-            fragment.show(getFragmentManager(), "show");
-            preferences.edit().putBoolean("Initialized", true).apply();
-        }
         //receive intent
         if (Intent.ACTION_VIEW.equals(getIntent().getAction())) {
             Bundle bundle = new Bundle();
@@ -40,24 +29,6 @@ public class Show extends Activity {
             fragmentTransaction.commit();
         } else {
             Toast.makeText(this, "Intent Error!", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    public static class InitDialogFragment extends DialogFragment {
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setTitle(getString(R.string.initial_dialog_title))
-                    .setMessage(getString(R.string.initial_dialog))
-                    .setNeutralButton(getString(R.string.initial_dialog_ok), new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Intent intent = new Intent(getActivity(), TOAuth.class);
-                            startActivity(intent);
-                        }
-                    })
-                    .setNegativeButton(getString(R.string.initial_dialog_no), null);
-            return builder.create();
         }
     }
 
