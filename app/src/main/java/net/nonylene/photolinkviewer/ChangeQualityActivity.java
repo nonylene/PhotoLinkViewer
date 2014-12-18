7a5ae04c1ed46b33b09966dc41194ec01277c0df
nonylene <cookie.speaker@gmail.com>
2014-12-18 23:49:05 +0900

    until build dialog

--- app/src/main/java/net/nonylene/photolinkviewer/ChangeQualityActivity.java
+++ app/src/main/java/net/nonylene/photolinkviewer/ChangeQualityActivity.java
@@ -2,10 +2,16 @@ package net.nonylene.photolinkviewer;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.preference.ListPreference;
+import android.preference.Preference;
 
 public class ChangeQualityActivity extends Activity {
 
@@ -16,27 +22,27 @@ public class ChangeQualityActivity extends Activity {
         ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
         actionBar.addTab(actionBar.newTab()
-        .setText("LTE")
-        .setTabListener(new MyTabListener(new LTEFragment()))
+                        .setText("LTE")
+                        .setTabListener(new MyTabListener(new LTEFragment()))
         );
         actionBar.addTab(actionBar.newTab()
-                .setText("Wifi")
-                .setTabListener(new MyTabListener(new WifiFragment()))
+                        .setText("Wifi")
+                        .setTabListener(new MyTabListener(new WifiFragment()))
         );
 
     }
 
 
-    class MyTabListener implements ActionBar.TabListener{
+    class MyTabListener implements ActionBar.TabListener {
         private Fragment fragment;
 
-        public MyTabListener (Fragment fragment){
+        public MyTabListener(Fragment fragment) {
             this.fragment = fragment;
         }
 
         @Override
         public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-            ft.add(R.id.content,fragment);
+            ft.add(R.id.content, fragment);
         }
 
         @Override
@@ -56,8 +62,44 @@ public class ChangeQualityActivity extends Activity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.quality_setting_3g);
+            Preference preference = findPreference("quality_3g_batch");
+            preference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    ListPreference listPreference = (ListPreference) findPreference("flickr_quality_3g");
+                    listPreference.setValue("original");
+                    BatchDialogFragment batchDialogFragment = new BatchDialogFragment();
+                    batchDialogFragment.setTargetFragment(LTEFragment.this, 1);
+                    batchDialogFragment.show(getFragmentManager(),"batch");
+                    return false;
+                }
+            });
+        }
+
+        public static class BatchDialogFragment extends DialogFragment {
+            @Override
+            public Dialog onCreateDialog(Bundle savedInstanceState) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                String[] items = {"original", "large", "medium"};
+                builder.setTitle(getString(R.string.retweet_dialog_title))
+                        .setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                getTargetFragment().onActivityResult(getTargetRequestCode(), which, null);
+                                dialog.dismiss();
+                            }
+                        });
+                return builder.create();
+            }
+
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
         }
     }
+
 
     public static class WifiFragment extends PreferenceSummaryFragment {
 
