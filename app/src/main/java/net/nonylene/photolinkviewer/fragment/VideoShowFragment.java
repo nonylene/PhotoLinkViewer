ebc3f0648f1f4e99df26951b25ff4772a2247f83
nonylene <cookie.speaker@gmail.com>
2015-01-26 08:07:29 +0900

    vine volley

--- app/src/main/java/net/nonylene/photolinkviewer/fragment/VideoShowFragment.java
+++ app/src/main/java/net/nonylene/photolinkviewer/fragment/VideoShowFragment.java
@@ -1,8 +1,6 @@
 package net.nonylene.photolinkviewer.fragment;
 
 import android.app.Fragment;
-import android.app.LoaderManager;
-import android.content.Loader;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -16,15 +14,17 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
 import net.nonylene.photolinkviewer.tool.Base49;
 import net.nonylene.photolinkviewer.R;
-import net.nonylene.photolinkviewer.async.AsyncJSON;
+import net.nonylene.photolinkviewer.tool.MyJsonObjectRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -42,78 +42,6 @@ public class VideoShowFragment extends Fragment {
         String url = getArguments().getString("url");
         URLPurser(url);
         return view;
-    }
-
-    public class AsyncJSONExecute implements LoaderManager.LoaderCallbacks<JSONObject> {
-
-
-        public void Start(String url) {
-            Bundle bundle = new Bundle();
-            bundle.putString("url", url);
-            getLoaderManager().restartLoader(0, bundle, this);
-        }
-
-        @Override
-        public Loader<JSONObject> onCreateLoader(int id, Bundle bundle) {
-            try {
-                String c = bundle.getString("url");
-                URL url = new URL(c);
-                return new AsyncJSON(getActivity().getApplicationContext(), url);
-            } catch (IOException e) {
-                Log.e("JSONLoaderError", e.toString());
-                return null;
-            }
-        }
-
-        @Override
-        public void onLoadFinished(Loader<JSONObject> loader, JSONObject json) {
-            final FrameLayout frameLayout = (FrameLayout) view.findViewById(R.id.videoshowframe);
-            final ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.videoshowprogress);
-            if (json != null) {
-
-                try {
-                    Log.v("json", json.toString(2));
-                    JSONObject data = json.getJSONObject("data");
-                    JSONObject records = data.getJSONArray("records").getJSONObject(0);
-                    String file = records.getString("videoUrl");
-                    Log.v("URL", file);
-                    // view video
-                    final VideoView videoView = (VideoView) view.findViewById(R.id.videoview);
-                    videoView.setVideoURI(Uri.parse(file));
-                    // touch to stop
-                    videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                        @Override
-                        public void onPrepared(MediaPlayer mp) {
-                            //remove progressbar
-                            frameLayout.removeView(progressBar);
-                            videoView.start();
-                        }
-                    });
-                    // loop
-                    videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                        @Override
-                        public void onCompletion(MediaPlayer mp) {
-                            videoView.start();
-                        }
-                    });
-                    VideoOnTouchListener videoOnTouchListener = new VideoOnTouchListener();
-                    videoView.setOnTouchListener(videoOnTouchListener);
-                } catch (JSONException e) {
-                    Log.e("JSONError", e.toString());
-                    frameLayout.removeView(progressBar);
-                    Toast.makeText(getActivity(), getString(R.string.vine_json_toast), Toast.LENGTH_LONG).show();
-                }
-            } else {
-                frameLayout.removeView(progressBar);
-                Toast.makeText(getActivity(), getString(R.string.vine_url_toast), Toast.LENGTH_LONG).show();
-
-            }
-        }
-
-        @Override
-        public void onLoaderReset(Loader<JSONObject> loader) {
-
-        }
     }
 
     class VideoOnTouchListener implements View.OnTouchListener {
@@ -148,12 +76,63 @@ public class VideoShowFragment extends Fragment {
             Log.v("Vine", id);
             String request = "https://api.vineapp.com/timelines/posts/" + id;
             Log.v("vineAPI", request);
-            AsyncJSONExecute hoge = new AsyncJSONExecute();
-            hoge.Start(request);
+            // volley
+            RequestQueue queue = Volley.newRequestQueue(getActivity());
+            queue.add(new MyJsonObjectRequest(getActivity(), request,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            parseVine(response);
+                        }
+                    }
+            ));
         } catch (
                 Exception e
                 ) {
             Log.e("IOException", e.toString());
+        }
+    }
+
+    private void parseVine(JSONObject json){
+        final FrameLayout frameLayout = (FrameLayout) view.findViewById(R.id.videoshowframe);
+        final ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.videoshowprogress);
+        if (json != null) {
+
+            try {
+                Log.v("json", json.toString(2));
+                JSONObject data = json.getJSONObject("data");
+                JSONObject records = data.getJSONArray("records").getJSONObject(0);
+                String file = records.getString("videoUrl");
+                Log.v("URL", file);
+                // view video
+                final VideoView videoView = (VideoView) view.findViewById(R.id.videoview);
+                videoView.setVideoURI(Uri.parse(file));
+                // touch to stop
+                videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mp) {
+                        //remove progressbar
+                        frameLayout.removeView(progressBar);
+                        videoView.start();
+                    }
+                });
+                // loop
+                videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        videoView.start();
+                    }
+                });
+                VideoOnTouchListener videoOnTouchListener = new VideoOnTouchListener();
+                videoView.setOnTouchListener(videoOnTouchListener);
+            } catch (JSONException e) {
+                Log.e("JSONError", e.toString());
+                frameLayout.removeView(progressBar);
+                Toast.makeText(getActivity(), getString(R.string.vine_json_toast), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            frameLayout.removeView(progressBar);
+            Toast.makeText(getActivity(), getString(R.string.vine_url_toast), Toast.LENGTH_LONG).show();
         }
     }
 }
