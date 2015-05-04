128782dec68433db5282ce576b4e0ad54ccbbe53
nonylene <cookie.speaker@gmail.com>
2015-05-04 22:01:08 +0900

    set view with xml

---
+++ app/src/main/java/net/nonylene/photolinkviewer/tool/SquareNetworkImageView.java
@@ -0,0 +1,29 @@
+package net.nonylene.photolinkviewer.tool;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.android.volley.toolbox.NetworkImageView;
+
+public class SquareNetworkImageView extends NetworkImageView {
+
+    public SquareNetworkImageView(Context context) {
+        this(context, null);
+    }
+
+    public SquareNetworkImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SquareNetworkImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = getMeasuredWidth();
+        setMeasuredDimension(width, width);
+    }
+}
