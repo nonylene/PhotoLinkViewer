879bd2598f81a7b43ba3f8444ec515314ecbd8e0
nonylene <cookie.speaker@gmail.com>
2015-07-01 23:34:26 +0900

    progressbar and background tweak

--- app/src/main/java/net/nonylene/photolinkviewer/fragment/VideoShowFragment.java
+++ app/src/main/java/net/nonylene/photolinkviewer/fragment/VideoShowFragment.java
@@ -3,7 +3,6 @@ package net.nonylene.photolinkviewer.fragment;
 import android.app.Fragment;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -20,10 +19,13 @@ import android.widget.VideoView;
 
 import net.nonylene.photolinkviewer.R;
 import net.nonylene.photolinkviewer.tool.PLVUrl;
+import net.nonylene.photolinkviewer.tool.ProgressBarListener;
 
 public class VideoShowFragment extends Fragment {
     private View view;
     private ImageView imageView;
+    private FrameLayout videoShowFrameLayout;
+    private ProgressBar progressBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,9 +36,11 @@ public class VideoShowFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.videoshow_fragment, container, false);
         imageView = (ImageView) view.findViewById(R.id.video_image);
-        if (getArguments().getBoolean("single_frag", false)){
-            FrameLayout frameLayout = (FrameLayout)view.findViewById(R.id.videoshowframe);
-            frameLayout.setBackgroundColor(getResources().getColor(R.color.background));
+        videoShowFrameLayout = (FrameLayout) view.findViewById(R.id.videoshowframe);
+        progressBar = (ProgressBar) view.findViewById(R.id.show_progress);
+        if (getArguments().getBoolean("single_frag", false)) {
+            videoShowFrameLayout.setBackgroundResource(R.color.transparent);
+            // do not hide progressbar! progressbar of activity will be displayed under videoView.
         }
         PLVUrl plvUrl = getArguments().getParcelable("plvurl");
         playVideo(plvUrl);
@@ -71,8 +75,6 @@ public class VideoShowFragment extends Fragment {
     }
 
     private void playVideo(PLVUrl plvUrl) {
-        final FrameLayout frameLayout = (FrameLayout) view.findViewById(R.id.videoshowframe);
-        final ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.videoshowprogress);
         // view video
         final VideoView videoView = (VideoView) view.findViewById(R.id.videoview);
         videoView.setVideoURI(Uri.parse(plvUrl.getDisplayUrl()));
@@ -82,8 +84,8 @@ public class VideoShowFragment extends Fragment {
                 @Override
                 public void onPrepared(MediaPlayer mp) {
                     //remove progressbar
-                    frameLayout.removeView(progressBar);
-                    frameLayout.setBackgroundColor(getResources().getColor(R.color.background));
+                    removeProgressBar();
+                    videoShowFrameLayout.setBackgroundColor(getResources().getColor(R.color.background));
                     SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
                     videoView.setBackgroundColor(Color.TRANSPARENT);
                     if (preferences.getBoolean("video_play", true)) {
@@ -97,7 +99,7 @@ public class VideoShowFragment extends Fragment {
             VideoOnTouchListener videoOnTouchListener = new VideoOnTouchListener();
             videoView.setOnTouchListener(videoOnTouchListener);
 
-        }else{
+        } else {
             final MediaController mediaController = new MediaController(getActivity());
             videoView.setMediaController(mediaController);
             // touch to stop
@@ -105,8 +107,8 @@ public class VideoShowFragment extends Fragment {
                 @Override
                 public void onPrepared(MediaPlayer mp) {
                     //remove progressbar
-                    frameLayout.removeView(progressBar);
-                    frameLayout.setBackgroundColor(getResources().getColor(R.color.background));
+                    removeProgressBar();
+                    videoShowFrameLayout.setBackgroundColor(getResources().getColor(R.color.background));
                     SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
                     videoView.setBackgroundColor(Color.TRANSPARENT);
                     if (preferences.getBoolean("video_play", true)) {
@@ -140,5 +142,11 @@ public class VideoShowFragment extends Fragment {
                 mp.start();
             }
         });
+    }
+
+    private void removeProgressBar() {
+        videoShowFrameLayout.removeView(progressBar);
+        if (getActivity() instanceof ProgressBarListener)
+            ((ProgressBarListener) getActivity()).hideProgressBar();
     }
 }
