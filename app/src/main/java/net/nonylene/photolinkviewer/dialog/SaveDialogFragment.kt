c556be739e15cad1f49109e358656fa8026ba2d7
nonylene <cookie.speaker@gmail.com>
2015-10-05 20:44:17 +0900

    generate to kotlin

---
+++ app/src/main/java/net/nonylene/photolinkviewer/dialog/SaveDialogFragment.kt
@@ -0,0 +1,33 @@
+package net.nonylene.photolinkviewer.dialog
+
+import android.app.Dialog
+import android.app.DialogFragment
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+import net.nonylene.photolinkviewer.R
+
+class SaveDialogFragment : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // set custom view
+        val view = View.inflate(activity, R.layout.save_path, null)
+        (view.findViewById(R.id.path_TextView) as TextView).setText(arguments.getString("dir"))
+        (view.findViewById(R.id.path_EditText) as EditText).setText(arguments.getString("filename"))
+        return AlertDialog.Builder(activity).setView(view)
+                .setTitle(getString(R.string.save_dialog_title))
+                .setPositiveButton(getString(R.string.save_dialog_positive), DialogInterface.OnClickListener { dialogInterface, i ->
+                        // get filename
+                        arguments.putString("filename",
+                                (dialog.findViewById(R.id.path_EditText) as EditText).text.toString())
+                        targetFragment.onActivityResult(0, 0, Intent().putExtra("bundle", arguments))
+                    }
+                )
+                .setNegativeButton(getString(android.R.string.cancel), null)
+                .create()
+    }
+}
