f3009f0a6e7c02213fe8ef51f6194d3e7e6dc2e3
nonylene <cookie.speaker@gmail.com>
2015-10-11 20:37:21 +0900

    withindex

--- app/src/main/java/net/nonylene/photolinkviewer/tool/Base58.kt
+++ app/src/main/java/net/nonylene/photolinkviewer/tool/Base58.kt
@@ -7,10 +7,9 @@ object Base58 {
     private val CHARACTERS_LENGTH = BigInteger.valueOf(58)
 
     fun decode(encoded: String): String {
-        val reversed = encoded.reversed()
-        return (0..reversed.length() - 1).fold(BigInteger.valueOf(0)) { result, i ->
-            val alpha = BigInteger.valueOf(CHARACTERS.indexOf(reversed.get(i)).toLong())
-            result.add(alpha.multiply(CHARACTERS_LENGTH.pow(i)))
+        return encoded.reversed().withIndex().fold(BigInteger.valueOf(0)) { result, iv ->
+            val alpha = BigInteger.valueOf(CHARACTERS.indexOf(iv.value).toLong())
+            result.add(alpha.multiply(CHARACTERS_LENGTH.pow(iv.index)))
         }.toString()
     }
 }
