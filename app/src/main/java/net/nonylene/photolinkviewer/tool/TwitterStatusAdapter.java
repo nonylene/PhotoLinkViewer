828011ec9f5666a5fff015b46b596fbc15591788
nonylene <cookie.speaker@gmail.com>
2015-10-12 20:14:25 +0900

    move to custom view !

--- app/src/main/java/net/nonylene/photolinkviewer/tool/TwitterStatusAdapter.java
+++ app/src/main/java/net/nonylene/photolinkviewer/tool/TwitterStatusAdapter.java
@@ -1,19 +1,15 @@
 package net.nonylene.photolinkviewer.tool;
 
 import android.content.Context;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.android.volley.toolbox.ImageLoader;
 
 import net.nonylene.photolinkviewer.R;
+import net.nonylene.photolinkviewer.view.UserTweetLoadingView;
 import net.nonylene.photolinkviewer.view.UserTweetView;
 
 import org.jetbrains.annotations.NotNull;
@@ -23,21 +19,15 @@ import java.util.List;
 
 import twitter4j.Status;
 
-public class TwitterStatusAdapter extends BaseAdapter implements UserTweetView.TwitterViewListener {
+public class TwitterStatusAdapter extends BaseAdapter implements UserTweetView.TwitterViewListener , UserTweetLoadingView.LoadingViewListener{
 
     private List<Status> statusList = new ArrayList<>();
     private TwitterAdapterListener twitterAdapterListener;
     private ImageLoader imageLoader;
-    private Animation rotateAnimation;
     private boolean isRequesting;
-    private int selectableBackgroundId;
 
     public TwitterStatusAdapter(Context context, ImageLoader imageLoader) {
-        rotateAnimation = AnimationUtils.loadAnimation(context, R.anim.rotate);
         this.imageLoader = imageLoader;
-        TypedValue outValue = new TypedValue();
-        context.getTheme().resolveAttribute(android.R.attr.selectableItemBackground, outValue, true);
-        selectableBackgroundId = outValue.resourceId;
     }
 
     @Override
@@ -48,17 +38,6 @@ public class TwitterStatusAdapter extends BaseAdapter implements UserTweetView.T
     @Override
     public void onVideoShowFragmentRequired(@NotNull PLVUrl plvUrl) {
         twitterAdapterListener.onVideoShowFragmentRequired(plvUrl);
-    }
-
-    private class LoadingViewHolder {
-        LinearLayout baseView;
-        ImageView loadingView;
-
-        public void setView(View baseView) {
-            this.baseView = (LinearLayout) baseView;
-            loadingView = (ImageView) baseView.findViewById(R.id.loading_imageview);
-        }
-
     }
 
     @Override
@@ -110,41 +89,11 @@ public class TwitterStatusAdapter extends BaseAdapter implements UserTweetView.T
             }
 
         } else if (getItemViewType(position) == ItemType.LOADING.getId()) {
-            if (convertView == null) {
-                convertView = inflater.inflate(R.layout.loading_layout, parent, false);
-                LoadingViewHolder loadingViewHolder = new LoadingViewHolder();
-                loadingViewHolder.setView(convertView);
-                convertView.setTag(loadingViewHolder);
-            }
+            if (convertView == null) convertView = inflater.inflate(R.layout.loading_layout, parent, false);
 
-            final LoadingViewHolder loadingViewHolder = (LoadingViewHolder) convertView.getTag();
-
-            if (isRequesting) {
-                // loading now...
-                convertView.setBackgroundResource(android.R.color.transparent);
-                loadingViewHolder.loadingView.setAnimation(rotateAnimation);
-
-                convertView.setOnClickListener(null);
-
-            } else {
-                // not loading now...
-                convertView.setBackgroundResource(selectableBackgroundId);
-                loadingViewHolder.loadingView.setAnimation(null);
-
-                convertView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (!isRequesting) {
-                            isRequesting = true;
-                            // unset loading callback and background
-                            loadingViewHolder.baseView.setBackgroundResource(android.R.color.transparent);
-                            loadingViewHolder.loadingView.setAnimation(rotateAnimation);
-                            if (twitterAdapterListener != null)
-                                twitterAdapterListener.onReadMoreClicked();
-                        }
-                    }
-                });
-            }
+            UserTweetLoadingView userTweetLoadingView = (UserTweetLoadingView) convertView;
+            userTweetLoadingView.setLoadingViewListener(this);
+            userTweetLoadingView.setIsRequesting(isRequesting);
         }
 
         return convertView;
@@ -179,6 +128,12 @@ public class TwitterStatusAdapter extends BaseAdapter implements UserTweetView.T
 
     public void removeLoadingItem() {
         statusList.remove(statusList.size() - 1);
+    }
+
+    @Override
+    public void onReadMoreClicked() {
+        twitterAdapterListener.onReadMoreClicked();
+        setRequesting(true);
     }
 
     private enum ItemType {
---
+++ app/src/main/java/net/nonylene/photolinkviewer/view/UserTweetLoadingView.kt
@@ -0,0 +1,56 @@
+package net.nonylene.photolinkviewer.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.LinearLayout
+import net.nonylene.photolinkviewer.R
+
+class UserTweetLoadingView(context: Context, attr: AttributeSet?) : LinearLayout(context, attr){
+
+    private val rotateAnimation : Animation = AnimationUtils.loadAnimation(context, R.anim.rotate)
+    private val selectableBackgroundId : Int
+
+    private var loadingView : ImageView? = null
+
+    public var loadingViewListener: LoadingViewListener? = null
+
+    init {
+        val outValue = TypedValue();
+        context.theme.resolveAttribute(android.R.attr.selectableItemBackground, outValue, true);
+        selectableBackgroundId = outValue.resourceId;
+    }
+
+    protected override fun onFinishInflate() {
+        super.onFinishInflate()
+        loadingView = findViewById(R.id.loading_imageview) as ImageView
+    }
+
+    public fun setIsRequesting(isRequesting : Boolean) {
+        if (isRequesting) {
+            // loading now...
+            setBackgroundResource(android.R.color.transparent);
+            loadingView!!.animation = rotateAnimation;
+            setOnClickListener(null);
+        } else {
+            // not loading now...
+            setBackgroundResource(selectableBackgroundId);
+            loadingView!!.animation = null;
+
+            setOnClickListener {
+                if (!isRequesting) {
+                    setIsRequesting(true)
+                    // unset loading callback and background
+                    loadingViewListener?.onReadMoreClicked();
+                }
+            }
+        }
+    }
+
+    public interface LoadingViewListener {
+        fun onReadMoreClicked()
+    }
+}
