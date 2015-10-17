d1e1ad076d108f49f9a984f765083fff7dd5cf30
nonylene <cookie.speaker@gmail.com>
2015-10-17 17:52:34 +0900

    kotlin and volley

---
+++ app/src/main/java/net/nonylene/photolinkviewer/async/AsyncHttpBitmap.kt
@@ -0,0 +1,82 @@
+package net.nonylene.photolinkviewer.async
+
+import android.content.AsyncTaskLoader
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import com.squareup.okhttp.Request
+import net.nonylene.photolinkviewer.tool.OkHttpManager
+
+import net.nonylene.photolinkviewer.tool.PLVUrl
+
+import java.io.IOException
+
+class AsyncHttpBitmap(context: Context, private val plvUrl: PLVUrl, private val max_size: Int) : AsyncTaskLoader<AsyncHttpBitmap.Result>(context) {
+
+    private var result: Result? = null
+
+    override fun loadInBackground(): Result {
+        val httpResult = Result()
+
+        try {
+            val request = OkHttpManager.getOkHttpClient(context).newCall(
+                    Request.Builder()
+                            .url(plvUrl.displayUrl)
+                            .get()
+                            .build()
+            ).execute()
+
+            val inputStream = request.body().byteStream()
+
+            // if bitmap size is bigger than limit, load small photo
+            val bitmap: Bitmap
+            if (max_size < Math.max(plvUrl.height, plvUrl.width)) {
+                val size = Math.max(plvUrl.height, plvUrl.width) / max_size + 1
+                val options2 = BitmapFactory.Options()
+                options2.inSampleSize = size
+                bitmap = BitmapFactory.decodeStream(inputStream, null, options2)
+            } else {
+                bitmap = BitmapFactory.decodeStream(inputStream)
+            }
+            inputStream.close()
+            httpResult.bitmap = bitmap
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return httpResult
+    }
+
+    override fun deliverResult(httpResult: Result) {
+        if (isReset) {
+            this.result = null
+            return
+        }
+
+        this.result = httpResult
+        if (isStarted) super.deliverResult(httpResult)
+    }
+
+    public override fun onStartLoading() {
+        this.result?.let { deliverResult(it) }
+        if (takeContentChanged() || this.result == null) {
+            forceLoad()
+        }
+    }
+
+    public override fun onStopLoading() {
+        super.onStopLoading()
+        cancelLoad()
+    }
+
+    public override fun onReset() {
+        super.onReset()
+        onStopLoading()
+    }
+
+    inner class Result {
+        var bitmap: Bitmap? = null
+        var url: String? = null
+    }
+}
