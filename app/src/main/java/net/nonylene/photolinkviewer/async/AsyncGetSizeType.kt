dc1245ea07aff192f23b6b4c608201ee4e8038e6
nonylene <cookie.speaker@gmail.com>
2015-10-17 17:53:04 +0900

    use okhttp

--- app/src/main/java/net/nonylene/photolinkviewer/async/AsyncGetSizeType.kt
+++ app/src/main/java/net/nonylene/photolinkviewer/async/AsyncGetSizeType.kt
@@ -1,14 +1,15 @@
 package net.nonylene.photolinkviewer.async
 
+import android.content.Context
 import android.graphics.BitmapFactory
 import android.os.AsyncTask
+import com.squareup.okhttp.Request
+import net.nonylene.photolinkviewer.tool.OkHttpManager
 
 import java.io.IOException
-import java.net.HttpURLConnection
-import java.net.URL
 import java.util.Arrays
 
-open class AsyncGetSizeType : AsyncTask<String, Int, AsyncGetSizeType.Result>() {
+open class AsyncGetSizeType(private val context: Context) : AsyncTask<String, Int, AsyncGetSizeType.Result>() {
     private val png = byteArrayOf(137.toByte(), 80.toByte(), 78.toByte(), 71.toByte())
     private val gif = byteArrayOf(71.toByte(), 73.toByte(), 70.toByte(), 56.toByte())
     private val jpg = byteArrayOf(255.toByte(), 216.toByte(), 255.toByte())
@@ -17,33 +18,39 @@ open class AsyncGetSizeType : AsyncTask<String, Int, AsyncGetSizeType.Result>() 
     override fun doInBackground(vararg params: String): Result {
         val result = Result()
 
+        val client = OkHttpManager.getOkHttpClient(context)
+
+        val request = Request.Builder()
+                            .url(params[0])
+                            .get()
+                            .build()
+
         try {
-            // read binary and get type
-            val yaBinary = ByteArray(4)
-
-            val url = URL(params[0])
-            val urlConnection = url.openConnection() as HttpURLConnection
-
-            urlConnection.responseCode.let { code ->
-                if (code < 200 || code >= 300) {
-                    result.errorMessage =  "${code} - ${urlConnection.responseMessage}"
+            client.newCall(request).execute().let { response ->
+                if (!response.isSuccessful) {
+                    result.errorMessage =  "${response.code()} - ${response.message()}"
                     return result
                 }
-            }
 
-            urlConnection.inputStream.let {
-                it.read(yaBinary, 0, 4)
-                it.close()
+                // read binary and get type
+                val yaBinary = ByteArray(4)
+
+                response.body().byteStream().let {
+                    it.read(yaBinary, 0, 4)
+                    it.close()
+                }
+
+                result.type = getFileType(yaBinary)
             }
 
             // get bitmap size
-            val options = BitmapFactory.Options()
-            options.inJustDecodeBounds = true
-            BitmapFactory.decodeStream(url.openStream(), null, options)
+            BitmapFactory.Options().let { options ->
+                options.inJustDecodeBounds = true
+                BitmapFactory.decodeStream(client.newCall(request).execute().body().byteStream(), null, options)
 
-            result.type = getFileType(yaBinary)
-            result.height = options.outHeight
-            result.width = options.outWidth
+                result.height = options.outHeight
+                result.width = options.outWidth
+            }
 
         } catch (e: IOException) {
             e.printStackTrace()
