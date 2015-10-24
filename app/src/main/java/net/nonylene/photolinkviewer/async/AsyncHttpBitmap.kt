8dcc163ceb2995a2af42bc83e5bbd4e741631af5
nonylene <cookie.speaker@gmail.com>
2015-10-24 22:53:48 +0900

    merge async by bufferedInputStream

--- app/src/main/java/net/nonylene/photolinkviewer/async/AsyncHttpBitmap.kt
+++ app/src/main/java/net/nonylene/photolinkviewer/async/AsyncHttpBitmap.kt
@@ -8,37 +8,61 @@ import com.squareup.okhttp.Request
 import net.nonylene.photolinkviewer.tool.OkHttpManager
 
 import net.nonylene.photolinkviewer.tool.PLVUrl
+import java.io.BufferedInputStream
 
 import java.io.IOException
+import java.util.*
 
 class AsyncHttpBitmap(context: Context, private val plvUrl: PLVUrl, private val max_size: Int) : AsyncTaskLoader<AsyncHttpBitmap.Result>(context) {
 
     private var result: Result? = null
+    private val png = byteArrayOf(137.toByte(), 80.toByte(), 78.toByte(), 71.toByte())
+    private val gif = byteArrayOf(71.toByte(), 73.toByte(), 70.toByte(), 56.toByte())
+    private val jpg = byteArrayOf(255.toByte(), 216.toByte(), 255.toByte())
+    private val bmp = byteArrayOf(66.toByte(), 77.toByte())
 
     override fun loadInBackground(): Result {
         val httpResult = Result()
 
         try {
-            val request = OkHttpManager.getOkHttpClient(context).newCall(
+            val response = OkHttpManager.getOkHttpClient(context).newCall(
                     Request.Builder()
                             .url(plvUrl.displayUrl)
                             .get()
                             .build()
             ).execute()
 
-            val inputStream = request.body().byteStream()
+            if (!response.isSuccessful) {
+                httpResult.errorMessage =  "${response.code()} - ${response.message()}"
+                return httpResult
+            }
+
+            val inputStream = response.body().byteStream().let {
+                if (it.markSupported()) it else BufferedInputStream(it)
+            }
+
+            inputStream.mark(65536);
+
+            // get bitmap size
+            val options = BitmapFactory.Options().apply {
+                inJustDecodeBounds = true
+            }
+            BitmapFactory.decodeStream(inputStream, null, options)
+            inputStream.reset()
+
+            // read binary and get type
+            val yaBinary = ByteArray(4)
+            inputStream.read(yaBinary, 0, 4)
+            httpResult.type = getFileType(yaBinary)
+            inputStream.reset()
 
             // if bitmap size is bigger than limit, load small photo
             val bitmap: Bitmap?
             if (max_size < Math.max(plvUrl.height, plvUrl.width)) {
                 val options2 = BitmapFactory.Options().apply {
-                    inSampleSize = Math.max(plvUrl.height, plvUrl.width) / max_size + 1
+                    inSampleSize = Math.max(options.outHeight, options.outWidth) / max_size + 1
                 }
-                httpResult.apply {
-                    isResized = true
-                    originalWidth = plvUrl.height
-                    originalHeight = plvUrl.width
-                }
+                httpResult.isResized = true
                 bitmap = BitmapFactory.decodeStream(inputStream, null, options2)
             } else {
                 bitmap = BitmapFactory.decodeStream(inputStream)
@@ -70,6 +94,16 @@ class AsyncHttpBitmap(context: Context, private val plvUrl: PLVUrl, private val 
         }
     }
 
+    private fun getFileType(head: ByteArray): String {
+        return when {
+            Arrays.equals(Arrays.copyOfRange(head, 0, 4), png) -> "png"
+            Arrays.equals(Arrays.copyOfRange(head, 0, 4), gif) -> "gif"
+            Arrays.equals(Arrays.copyOfRange(head, 0, 3), jpg) -> "jpg"
+            Arrays.equals(Arrays.copyOfRange(head, 0, 2), bmp) -> "bmp"
+            else                                               -> "unknown"
+        }
+    }
+
     public override fun onStopLoading() {
         super.onStopLoading()
         cancelLoad()
@@ -86,5 +120,7 @@ class AsyncHttpBitmap(context: Context, private val plvUrl: PLVUrl, private val 
         var originalWidth: Int = 0
         var originalHeight: Int = 0
         var isResized: Boolean = false
+        var type: String? = null
+        var errorMessage: String? = null
     }
 }
