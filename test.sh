1b4e4eafc11da776516272a094ded138b92c38c4
nonylene <cookie.speaker@gmail.com>
2015-11-12 02:22:29 +0900

    fix

--- test.sh
+++ test.sh
@@ -8,7 +8,8 @@ do
     echo $n test starting...
     nohup bash -c "case $CIRCLE_NODE_INDEX in 0) $ANDROID_HOME/tools/emulator -avd test22 -no-skin -no-boot-anim -no-audio -no-window ;; 1) $ANDROID_HOME/tools/emulator -avd test22 -no-skin -no-boot-anim -no-audio -no-window ;; 2) $ANDROID_HOME/tools/emulator -avd test22 -no-skin -no-boot-anim -no-audio -no-window ;; 3) $ANDROID_HOME/tools/emulator -avd test22 -no-skin -no-boot-anim -no-audio -no-window ;; esac &"
     circle-android wait-for-boot
-    ./gradlew --info --stacktrace clean :app:connectedAndroidTest && break    c=$?
+    ./gradlew --info --stacktrace clean :app:connectedAndroidTest && break
+    c=$?
     echo $n test errored.
     adb emu kill
     sleep 60
