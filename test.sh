7c28ab4dd318539c69b26980de0e8b3a1c056ffc
nonylene <cookie.speaker@gmail.com>
2015-11-12 02:49:10 +0900

    Revert "test commit"
    This reverts commit 1046b7a5f1f062b93847777987be3857bc38331a.
    
    # Conflicts:
    #	test.sh

--- test.sh
+++ test.sh
@@ -6,15 +6,11 @@ until [ $n -ge 2 ]
 do
     n=$[$n+1]
     echo $n test starting...
-    nohup bash -c "case $CIRCLE_NODE_INDEX in 0) $ANDROID_HOME/tools/emulator -avd test22 -no-skin -no-boot-anim -no-audio -no-window ;; 1) $ANDROID_HOME/tools/emulator -avd test22 -no-skin -no-boot-anim -no-audio -no-window ;; 2) $ANDROID_HOME/tools/emulator -avd test22 -no-skin -no-boot-anim -no-audio -no-window ;; 3) $ANDROID_HOME/tools/emulator -avd test22 -no-skin -no-boot-anim -no-audio -no-window ;; esac &"
     circle-android wait-for-boot
-    ./gradlew --info --stacktrace clean :app:connectedAndroidTest && break
+    ./gradlew --info --stacktrace clean :app:connectedAndroidTest && break      # substitute your command here
     c=$?
     echo $n test errored.
-    adb emu kill
-    sleep 60
-    adb kill-server
-    sleep 30
+    sleep 15
 done
 
 echo tested $n times.
