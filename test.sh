df1b7318369613a50fe683bb657f144b57124d5c
nonylene <cookie.speaker@gmail.com>
2015-11-12 02:51:01 +0900

    timing

--- test.sh
+++ test.sh
@@ -6,11 +6,12 @@ until [ $n -ge 2 ]
 do
     n=$[$n+1]
     echo $n test starting...
-    circle-android wait-for-boot
     ./gradlew --info --stacktrace clean :app:connectedAndroidTest && break      # substitute your command here
     c=$?
     echo $n test errored.
-    sleep 15
+    adb reboot
+    sleep 50
+    circle-android wait-for-boot
 done
 
 echo tested $n times.
