4632b60285b4843fb2e619712a0fea1ea3006956
nonylene <cookie.speaker@gmail.com>
2015-11-11 23:05:02 +0900

    improve fail

--- test.sh
+++ test.sh
@@ -1,9 +1,13 @@
 #! /usr/bin/env bash
 
+c=0
 n=0
 until [ $n -ge 2 ]
 do
-	./gradlew --info --stacktrace clean :app:connectedAndroidTest && break      # substitute your command here
-	n=$[$n+1]
-	sleep 15
+    ./gradlew --info --stacktrace clean :app:connectedAndroidTest && break      # substitute your command here
+    c=$?
+    n=$[$n+1]
+    sleep 15
 done
+
+exit $c
