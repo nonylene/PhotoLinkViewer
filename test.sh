1046b7a5f1f062b93847777987be3857bc38331a
nonylene <cookie.speaker@gmail.com>
2015-11-12 01:27:43 +0900

    test commit

--- test.sh
+++ test.sh
@@ -6,10 +6,14 @@ until [ $n -ge 2 ]
 do
     n=$[$n+1]
     echo $n test starting...
+    nohup bash -c "case $CIRCLE_NODE_INDEX in 0) $ANDROID_HOME/tools/emulator -avd test22 -no-skin -no-boot-anim -no-audio -no-window ;; 1) $ANDROID_HOME/tools/emulator -avd test22 -no-skin -no-boot-anim -no-audio -no-window ;; 2) $ANDROID_HOME/tools/emulator -avd test22 -no-skin -no-boot-anim -no-audio -no-window ;; 3) $ANDROID_HOME/tools/emulator -avd test22 -no-skin -no-boot-anim -no-audio -no-window ;; esac &"
+    circle-android wait-for-boot
+    sleep 30
     ./gradlew --info --stacktrace clean :app:connectedAndroidTest && break      # substitute your command here
     c=$?
     echo $n test errored.
-    sleep 15
+    adb emu kill
+    sleep 30
 done
 
 echo tested $n times.
