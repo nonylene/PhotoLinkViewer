8122eb9f3b7edba0a5e1e152e27775d346adf673
nonylene <cookie.speaker@gmail.com>
2015-11-11 23:23:48 +0900

    detail

--- test.sh
+++ test.sh
@@ -4,10 +4,14 @@ c=0
 n=0
 until [ $n -ge 2 ]
 do
+    n=$[$n+1]
+    echo $n test starting...
     ./gradlew --info --stacktrace clean :app:connectedAndroidTest && break      # substitute your command here
     c=$?
-    n=$[$n+1]
+    echo $n test finished.
     sleep 15
 done
+
+echo tested $n times.
 
 exit $c
