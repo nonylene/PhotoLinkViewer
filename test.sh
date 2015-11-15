005958fffd1193cc2ecb21dec4cdd7ce2f31b58d
nonylene <cookie.speaker@gmail.com>
2015-11-15 23:27:51 +0900

    test various versions

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ port=5554
 until [ $n -ge 3 ]
 do
     c=0
-    case $CIRCLE_NODE_INDEX in 0) export ANDROID_VERSION=22 ;; 1) export ANDROID_VERSION=22 ;; 2) export ANDROID_VERSION=22 ;; 3) ANDROID_VERSION=22 ;; esac
+    case $CIRCLE_NODE_INDEX in 0) export ANDROID_VERSION=22 ;; 1) export ANDROID_VERSION=19 ;; 2) export ANDROID_VERSION=17 ;; 3) ANDROID_VERSION=15 ;; esac
     nohup bash -c "$ANDROID_HOME/tools/emulator -avd test$ANDROID_VERSION -no-skin -no-boot-anim -no-audio -no-window -port $port &"
     circle-android wait-for-boot
     sleep 30
