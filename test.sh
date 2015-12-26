6a71ff66d8d918000ee0eabb91cb99123f1508f2
nonylene <cookie.speaker@gmail.com>
2015-12-26 18:51:26 +0900

    set to quiet log

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ do
     sleep 30
     n=$[$n+1]
     echo $n test starting...
-    ./gradlew --stacktrace clean :app:connectedAndroidTest && break      # substitute your command here
+    ./gradlew --quiet --stacktrace clean :app:connectedAndroidTest && break      # substitute your command here
     c=$?
     echo $n test errored.
     adb emu kill
